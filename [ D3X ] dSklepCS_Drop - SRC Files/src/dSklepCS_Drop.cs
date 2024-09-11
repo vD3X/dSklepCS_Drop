1b589f67b91dc1b5835f31835b84fe9c09a06be7
D3X <jakub.bartosik68@gmail.com>
2024-09-11 19:39:24 +0200

    Changed plugin version

--- [ D3X ] dSklepCS_Drop - SRC Files/src/dSklepCS_Drop.cs
+++ [ D3X ] dSklepCS_Drop - SRC Files/src/dSklepCS_Drop.cs
@@ -13,7 +13,7 @@ public class dSklepCS_Drop : BasePlugin
     public override string ModuleName => "[CS2] D3X - [ dSklepCS Drop wPLN ]";
     public override string ModuleAuthor => "D3X";
     public override string ModuleDescription => "Plugin na możliwość dropu wPLN do sklepCS na serwery CS2 by D3X";
-    public override string ModuleVersion => "1.0.0";
+    public override string ModuleVersion => "1.0.1";
 
     public static dSklepCS_Drop Instance { get; set; } = new();
 
